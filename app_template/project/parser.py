c324a7829db0eab7d593568f65d989b2890e476f
hdoupe <henrymdoupe@gmail.com>
2018-12-10 15:04:09 -0500

    Add parse_parameters to template class

--- app_template/project/parser.py
+++ app_template/project/parser.py
@@ -9,3 +9,10 @@ class {Project}Parser(Parser):
     if they exist.
     """
     displayer_class = {Project}Displayer
+
+    def parse_parameters(self):
+        params, jsonparams, errors_warnings = super().parse_parameters()
+        # parse the params, jsonparams, and errors_warnings further
+
+        # done parsing
+        return params, jsonparams, errors_warnings
