350fec9b3606ff7144cb137c5e9036cc722bc085
hdoupe <henrymdoupe@gmail.com>
2019-02-27 20:46:50 -0500

    Add preview with markdown rendering and syntax highlighting

--- static/js/publish.js
+++ static/js/publish.js
@@ -1,11 +1,42 @@
 "use strict";
 
+axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
+axios.defaults.xsrfCookieName = "csrftoken";
+
 var BrowserRouter = ReactRouterDOM.BrowserRouter;
 var Route = ReactRouterDOM.Route;
 var Switch = ReactRouterDOM.Switch;
 
-axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
-axios.defaults.xsrfCookieName = "csrftoken";
+var md = new Remarkable({
+  highlight: function(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return hljs.highlight(lang, str).value;
+      } catch (err) {}
+    }
+
+    try {
+      return hljs.highlightAuto(str).value;
+    } catch (err) {}
+
+    return ""; // use external default escaping
+  }
+});
+
+function markdownElement(markdownText) {
+  const marked = {
+    __html: md.render(markdownText)
+  };
+  return (
+    <div className="markdown-wrapper">
+      <div
+        dangerouslySetInnerHTML={marked}
+        className="content card publish markdown"
+        style={{ width: "50rem" }}
+      />
+    </div>
+  );
+}
 
 class TextField extends React.Component {
   constructor(props) {
@@ -13,21 +44,31 @@ class TextField extends React.Component {
   }
 
   render() {
+    var element;
+    if (this.props.preview) {
+      element = markdownElement(this.props.value);
+    } else {
+      element = (
+        <input
+          className="form-control"
+          name={this.props.name}
+          type={this.props.type}
+          value={this.props.value}
+          placeholder={this.props.placeholder}
+          onChange={this.props.handleChange}
+          required
+        />
+      );
+    }
     return (
-      <p>
-        <label>
-          <b>{this.props.label}:</b>
-          <input
-            className="form-control"
-            name={this.props.name}
-            type={this.props.type}
-            value={this.props.value}
-            placeholder={this.props.placeholder}
-            onChange={this.props.handleChange}
-            required
-          />
-        </label>
-      </p>
+      <div>
+        <p>
+          <label>
+            <b>{this.props.label}:</b>
+            {element}
+          </label>
+        </p>
+      </div>
     );
   }
 }
@@ -40,21 +81,29 @@ class Description extends React.Component {
   render() {
     const description = this.props.description;
     const charsLeft = 1000 - description.length;
+    var element;
+    if (this.props.preview) {
+      element = markdownElement(description);
+    } else {
+      element = (
+        <textarea
+          className="form-control"
+          name="description"
+          type="text"
+          placeholder="What does this app do? Must be less than 1000 characters."
+          value={description}
+          onChange={this.props.handleChange}
+          maxLength="1000"
+          style={{ width: "50rem" }}
+          // required - shows up red before entering text on firefox?
+        />
+      );
+    }
     return (
       <p className="description">
         <label>
           <b>App overview:</b>
-          <textarea
-            className="form-control"
-            name="description"
-            type="text"
-            placeholder="What does this app do? Must be less than 1000 characters."
-            value={description}
-            onChange={this.props.handleChange}
-            maxLength="1000"
-            style={{ width: "50rem" }}
-            // required - shows up red before entering text on firefox?
-          />
+          {element}
         </label>
         <small>{charsLeft}</small>
       </p>
@@ -68,20 +117,32 @@ class CodeSnippet extends React.Component {
   }
 
   render() {
+    var element;
+    if (this.props.preview) {
+      const ticks = "```";
+      const markdownText = `${ticks}${this.props.language}\n${
+        this.props.code
+      }\n${ticks}`;
+      element = markdownElement(markdownText);
+    } else {
+      element = (
+        <textarea
+          className="form-control"
+          name={this.props.name}
+          type="text"
+          placeholder="# code snippet here"
+          value={this.props.code}
+          onChange={this.props.handleChange}
+          style={{ width: "50rem" }}
+          // required - shows up red before entering text on firefox?
+        />
+      );
+    }
     return (
       <p>
         <label>
           <b>{this.props.function_name + ":"}</b> {this.props.description}
-          <textarea
-            className="form-control"
-            name={this.props.name}
-            type="text"
-            placeholder="# code snippet here"
-            value={this.props.code}
-            onChange={this.props.handleChange}
-            style={{ width: "50rem" }}
-            // required - shows up red before entering text on firefox?
-          />
+          {element}
         </label>
       </p>
     );
@@ -139,12 +200,14 @@ class PublishForm extends React.Component {
       installation: "",
       server_ram: 4,
       server_cpu: 2,
-      exp_task_time: 0
+      exp_task_time: 0,
+      preview: this.props.preview
     };
 
     this.handleChange = this.handleChange.bind(this);
     this.handleServerSizeChange = this.handleServerSizeChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+    this.togglePreview = this.togglePreview.bind(this);
     this.componentDidMount = this.componentDidMount.bind(this);
   }
 
@@ -157,6 +220,12 @@ class PublishForm extends React.Component {
       server_ram: ram,
       server_cpu: cpu
     });
+  }
+
+  togglePreview() {
+    event.preventDefault();
+    console.log(this.state.preview);
+    this.setState({ preview: !this.state.preview });
   }
 
   componentDidMount() {
@@ -182,10 +251,12 @@ class PublishForm extends React.Component {
           value={this.state.name}
           type="text"
           placeholder="What's the name of this app?"
+          preview={this.state.preview}
         />
         <Description
           handleChange={this.handleChange}
           description={this.state.description}
+          preview={this.state.preview}
         />
         <h3>Python Functions</h3>
         <hr className="my-4" />
@@ -203,6 +274,8 @@ class PublishForm extends React.Component {
           name="package_defaults"
           description="Get the default Model Parameters and their meta data"
           code={this.state.package_defaults}
+          preview={this.state.preview}
+          language="python"
         />
         <CodeSnippet
           handleChange={this.handleChange}
@@ -210,6 +283,8 @@ class PublishForm extends React.Component {
           name="parse_user_adjustments"
           description="Do model-specific formatting and validation on the user adjustments"
           code={this.state.parse_user_adjustments}
+          preview={this.state.preview}
+          language="python"
         />
         <CodeSnippet
           handleChange={this.handleChange}
@@ -217,6 +292,8 @@ class PublishForm extends React.Component {
           name="run_simulation"
           description="Submit the user adjustments (or none) to the model to run the simulations"
           code={this.state.run_simulation}
+          preview={this.state.preview}
+          language="python"
         />
         <h3>Environment</h3>
         <hr className="my-4" />
@@ -235,6 +312,8 @@ class PublishForm extends React.Component {
           name="installation"
           description="Bash commands for installing this project"
           code={this.state.installation}
+          preview={this.state.preview}
+          language="bash"
         />
         <ServerSize
           handleServerSizeChange={this.handleServerSizeChange}
@@ -247,8 +326,17 @@ class PublishForm extends React.Component {
           name="exp_task_time"
           value={this.state.exp_task_time}
           type="number"
+          preview={false}
         />
-        <input type="submit" value="Submit" />
+        <button className="btn inline-block" onClick={this.togglePreview} value>
+          {this.state.preview ? "Edit" : "Preview"}
+        </button>
+        <div class="divider" />
+        <input
+          className="btn inline-block go-btn"
+          type="submit"
+          value={this.props.submitType}
+        />
       </form>
     );
   }
@@ -294,6 +382,8 @@ class AppDetail extends React.Component {
       <PublishForm
         fetch_init_state={this.fetch_init_state}
         fetch_on_submit={this.fetch_on_submit}
+        preview={true}
+        submitType="Update"
       />
     );
   }
@@ -324,6 +414,8 @@ class CreateApp extends React.Component {
       <PublishForm
         fetch_init_state={this.fetch_init_state}
         fetch_on_submit={this.fetch_on_submit}
+        preview={false}
+        submitType="Publish"
       />
     );
   }
