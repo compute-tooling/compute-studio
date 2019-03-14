de2f4eb337d87bd27c260f1445b30bd349e88cb4
hdoupe <henrymdoupe@gmail.com>
2019-03-14 15:21:34 -0400

    Remove app templating system

--- app_template/project/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-
-from .models import {Project}Inputs, {Project}Run
-
-admin.site.register({Project}Inputs)
-admin.site.register({Project}Run)
