34679543336ca40f51875d8cb18cd78d7f0cda39
hdoupe <henrymdoupe@gmail.com>
2019-03-18 18:55:45 -0400

    Get ready for tax-brain tags

--- webapp/apps/comp/tags.py
+++ webapp/apps/comp/tags.py
@@ -19,6 +19,7 @@ class TagOption:
 
 
 matchups_tags = {
+    "dimension": "Batters",
     "tags": [
         Tag(
             key="attribute",
@@ -60,4 +61,98 @@ matchups_tags = {
     ],
 }
 
-TAGS = {"Matchups": matchups_tags}
+taxbrain_tags = {
+    "dimension": "Years",
+    "tags": [
+        Tag(
+            key="table_type",
+            hidden=False,
+            values=[
+                TagOption(
+                    value="dist",
+                    title="Distribution Table",
+                    tooltip="Distribution tooltip",
+                    children=[
+                        Tag(
+                            key="law",
+                            hidden=True,
+                            values=[
+                                TagOption(
+                                    value="current",
+                                    title="Current Law",
+                                    active=True,
+                                    tooltip="base tooltip",
+                                ),
+                                TagOption(
+                                    value="reform",
+                                    title="Reform",
+                                    tooltip="reform tooltip",
+                                ),
+                            ],
+                        )
+                    ],
+                ),
+                TagOption(
+                    value="diff",
+                    title="Difference Table",
+                    tooltip="difference tooltip",
+                    active=True,
+                    children=[
+                        Tag(
+                            key="tax_type",
+                            hidden=False,
+                            values=[
+                                TagOption(
+                                    value="payroll",
+                                    title="Payroll Tax",
+                                    tooltip="income tooltip",
+                                ),
+                                TagOption(
+                                    value="ind_income",
+                                    title="Income Tax",
+                                    tooltip="income tooltip",
+                                ),
+                                TagOption(
+                                    value="combined",
+                                    title="Combined",
+                                    active=True,
+                                    tooltip="",
+                                ),
+                            ],
+                        )
+                    ],
+                ),
+            ],
+        ),
+        Tag(
+            key="grouping",
+            hidden=False,
+            values=[
+                TagOption(
+                    value="bins",
+                    title="Income Bins",
+                    active=True,
+                    tooltip="income bins tooltip",
+                ),
+                TagOption(
+                    value="deciles",
+                    title="Income Deciles",
+                    tooltip="income deciles tooltip",
+                ),
+            ],
+        ),
+    ],
+    "aggr_tags": [
+        Tag(
+            key="law",
+            hidden=False,
+            values=[
+                TagOption(value="current", title="Current Law"),
+                TagOption(value="reform", title="Reform"),
+                TagOption(value="change", title="Change", active=True),
+            ],
+        )
+    ],
+}
+
+TAGS = {"Matchups": matchups_tags, "Tax-Brain": taxbrain_tags}
