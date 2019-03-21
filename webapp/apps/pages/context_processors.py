e39c5f851deac1a58fdfc88e2f4a6f45d6b95265
hdoupe <henrymdoupe@gmail.com>
2019-03-21 13:06:18 -0400

    Automatically add projects to dropdown

---
+++ webapp/apps/pages/context_processors.py
@@ -0,0 +1,11 @@
+from webapp.apps.users.models import Project
+
+
+def project_list(request):
+    projects = Project.objects.all().order_by("owner__user__username", "title")
+    project_list = []
+    for project in projects:
+        project_list.append(
+            (project.owner.user.username, project.title, project.app_url)
+        )
+    return {"project_list": project_list}
--- webapp/settings.py
+++ webapp/settings.py
@@ -87,6 +87,7 @@ TEMPLATES = [
                 "django.template.context_processors.request",
                 "django.contrib.auth.context_processors.auth",
                 "django.contrib.messages.context_processors.messages",
+                "webapp.apps.pages.context_processors.project_list",
             ]
         },
     }
