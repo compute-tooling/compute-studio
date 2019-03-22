de5225fdd5814811d007e037f007ec7a5158c070
hdoupe <henrymdoupe@gmail.com>
2019-03-22 15:57:58 -0400

    Add blue tint to edited input boxes

--- static/js/core.js
+++ static/js/core.js
@@ -1,28 +1,36 @@
-
-$(document).ready(function(){
+$(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
 
-  $('.form-group > input.form-control').each(function() {
-    // add edited class to be used for graying out edited fields.
-    input = $(this)
-    value = input.val()
-    value_default = input.prop('placeholder');
-    value_changed = (value != '') && (value != value_default);
-    group = input.closest('.form-group')
+  var toggleEdited = function(input) {
+    value = input.val();
+    value_default = input.prop("placeholder");
+    value_changed = value != "" && value != value_default;
     if (value_changed) {
-      group.addClass('edited');
+      input.addClass("edited");
     } else {
-      input.val(''); // show placeholder instead of value entered that = default
-      group.removeClass('edited');
+      input.val(""); // show placeholder instead of value entered that = default
+      input.removeClass("edited");
     }
+  };
+
+  $("input.form-control").blur(function() {
+    // add edited class to be used for graying out edited fields.
+    input = $(this);
+    toggleEdited(input);
   });
 
-  $('select.form-control').change(function(e) {
+  $("input.form-control").each(function() {
+    // add edited class to be used for graying out edited fields.
+    input = $(this);
+    toggleEdited(input);
+  });
+
+  $("select.form-control").change(function(e) {
     console.log($(this));
     $(this).removeClass("unedited");
   });
 
-  $('#inputs-form').submit(function(e) {
+  $("#inputs-form").submit(function(e) {
     $(".unedited.select.form-control").each(function() {
       select = $(this);
       select.prop("disabled", true);
