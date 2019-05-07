94a73200b71268b576d930936230372ab921ece7
hdoupe <henrymdoupe@gmail.com>
2019-05-07 08:33:44 -0400

    Add tax-brain config and use repo url to locate app code and install

--- distributed/app_writer.py
+++ distributed/app_writer.py
@@ -8,7 +8,7 @@ from jinja2 import Template
 
 
 def clean(word):
-    return re.sub("[^0-9a-zA-Z]+", "*", word).lower()
+    return re.sub("[^0-9a-zA-Z]+", "", word).lower()
 
 
 if __name__ == "__main__":
@@ -29,21 +29,26 @@ if __name__ == "__main__":
         config = json.loads(f.read())
 
     ext_str = ""
-
+    reg_url = "https://github.com"
+    raw_url = "https://raw.githubusercontent.com"
     for obj in config:
-        owner, title, sim_time_limit = obj["owner"], obj["title"], obj["sim_time_limit"]
-        safeowner = clean(owner)
-        safetitle = clean(title)
+        safeowner = clean(obj["owner"])
+        safetitle = clean(obj["title"])
+        repo_url = obj["repo_url"]
+        raw_repo_url = repo_url.replace(reg_url, raw_url)
         out = f"docker-compose-apps/docker-compose.{safeowner}_{safetitle}.yml"
         ext_str += f" -f {out}"
         with open(out, "w") as f:
             f.write(
                 template.render(
-                    OWNER=owner,
-                    TITLE=title,
+                    OWNER=obj["owner"],
+                    TITLE=obj["title"],
+                    BRANCH=obj["branch"],
                     SAFEOWNER=safeowner,
                     SAFETITLE=safetitle,
-                    SIM_TIME_LIMIT=sim_time_limit,
+                    SIM_TIME_LIMIT=obj["sim_time_limit"],
+                    REPO_URL=repo_url,
+                    RAW_REPO_URL=raw_repo_url,
                     **obj["environment"],
                 )
             )
