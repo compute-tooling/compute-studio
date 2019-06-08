77b0a7f909c31c7aad0c28c085e755ff5cf02f83
hdoupe <henrymdoupe@gmail.com>
2019-06-08 16:57:36 -0400

    Use kubernetes for container orchestration

--- distributed/app_writer.py
+++ distributed/app_writer.py
@@ -1,10 +1,13 @@
 import argparse
 import json
+import os
 import re
 import shutil
 from pathlib import Path
 
 from jinja2 import Template
+
+TAG = os.environ.get("TAG", "")
 
 
 def clean(word):
@@ -14,6 +17,7 @@ def clean(word):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Write tasks modules from template.")
     parser.add_argument("--config", required=True)
+    parser.add_argument("--tag", required=False, default=TAG)
     args = parser.parse_args()
 
     path = Path("docker-compose-apps")
@@ -21,12 +25,17 @@ if __name__ == "__main__":
     shutil.rmtree(path / "*", ignore_errors=True)
 
     with open("docker-compose.template.yml", "r") as f:
-        template_text = f.read()
+        dc_template_text = f.read()
 
-    template = Template(template_text)
+    dc_template = Template(dc_template_text)
 
     with open(args.config, "r") as f:
         config = json.loads(f.read())
+
+    with open("app-deployment.template.yaml", "r") as f:
+        kube_template_text = f.read()
+
+    kube_template = Template(kube_template_text)
 
     ext_str = ""
     reg_url = "https://github.com"
@@ -40,7 +49,7 @@ if __name__ == "__main__":
         ext_str += f" -f {out}"
         with open(out, "w") as f:
             f.write(
-                template.render(
+                dc_template.render(
                     OWNER=obj["owner"],
                     TITLE=obj["title"],
                     BRANCH=obj["branch"],
@@ -52,5 +61,20 @@ if __name__ == "__main__":
                     **obj["environment"],
                 )
             )
+        for action in ["io", "sim"]:
+            kubeout = f"kubernetes/{safeowner}-{safetitle}-{action}-deployment.yaml"
+
+            with open(kubeout, "w") as f:
+                f.write(
+                    kube_template.render(
+                        OWNER=obj["owner"],
+                        TITLE=obj["title"],
+                        SAFEOWNER=safeowner,
+                        SAFETITLE=safetitle,
+                        ACTION=action,
+                        TAG=args.tag,
+                        **obj["environment"],
+                    )
+                )
 
     print(ext_str)
