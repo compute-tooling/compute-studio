5f36cabf47ed7144219028aee9cded66ae121be3
hdoupe <henrymdoupe@gmail.com>
2019-08-13 18:51:57 -0400

    Clean up ErrorBoundary a little

--- src/ErrorBoundary.js
+++ src/ErrorBoundary.js
@@ -4,33 +4,35 @@ import { Card } from "react-bootstrap";
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
     super(props);
-    console.log("error boundary");
     this.state = { error: null, errorInfo: null };
   }
 
   componentDidCatch(error, errorInfo) {
-    // You can also log the error to an error reporting service
-    console.log("did catch?", error, errorInfo);
-    console.log(process.env.NODE_ENV);
     this.setState({
       error: error,
       errorInfo: errorInfo
-    })
+    });
   }
 
   render() {
-    console.log("triggered!!!!", this.state.errorInfo)
     if (this.state.errorInfo) {
       // Error path
       return (
         <Card>
           <Card.Title>Something went wrong.</Card.Title>
           <Card.Body>
-            <details style={{ whiteSpace: 'pre-wrap' }}>
-              {this.state.error && this.state.error.toString()}
-              <br />
-              {this.state.errorInfo.componentStack}
-            </details>
+            {process.env.NODE_ENV === "production" ? (
+              <p>
+                Please notify COMP developers by opening an issue in the COMP
+                source code repository or emailing Hank.
+              </p>
+            ) : (
+              <details style={{ whiteSpace: "pre-wrap" }}>
+                {this.state.error && this.state.error.toString()}
+                <br />
+                {this.state.errorInfo.componentStack}
+              </details>
+            )}
           </Card.Body>
         </Card>
       );
