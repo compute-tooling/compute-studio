1c56503d53a2ef67156fda23ceb6bd0a22011b99
hdoupe <henrymdoupe@gmail.com>
2019-10-03 17:47:21 -0400

    hot fix for anon users hitting the rest api

--- webapp/apps/comp/permissions.py
+++ webapp/apps/comp/permissions.py
@@ -3,7 +3,11 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class RequiresActive(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user.profile and request.user.profile.is_active)
+        return bool(
+            request.user.is_authenticated
+            and request.user.profile
+            and request.user.profile.is_active
+        )
 
 
 class RequiresPayment(BasePermission):
