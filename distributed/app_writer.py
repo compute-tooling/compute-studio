70dad2349e934653016ba3a8d41f7f579ecaab86
hdoupe <henrymdoupe@gmail.com>
2019-10-21 15:59:34 -0400

    Refactor app_writer into a class

--- distributed/app_writer.py
+++ distributed/app_writer.py
@@ -5,14 +5,12 @@ import os
 import re
 import shutil
 import subprocess
+import time
 from pathlib import Path
 
-from jinja2 import Template
 
-
-CONTAINER_REGISTRY = "gcr.io"
 TAG = os.environ.get("TAG", "")
-PROJECT = os.environ.get("PROJECT", "comp-workers")
+PROJECT = os.environ.get("PROJECT", "cs-workers-dev")
 
 
 def clean(word):
@@ -20,145 +18,196 @@ def clean(word):
 
 
 def run(cmd):
-    return subprocess.run(cmd, shell=True, check=True)
+    print(f"Running: {cmd}\n")
+    s = time.time()
+    res = subprocess.run(cmd, shell=True, check=True)
+    f = time.time()
+    print(f"\n\tFinished in {f-s} seconds.\n")
+    return res
 
 
-def build_base_images(project, tag):
-    run("docker build -t distributed:latest -f dockerfiles/Dockerfile ./")
-    run("docker build -t celerybase:latest -f dockerfiles/Dockerfile.celerybase ./")
-    run(f"docker build -t flask:{tag} -f dockerfiles/Dockerfile.flask ./")
+class ComputeStudioCluster:
 
-    for img_name in ["distributed", "celerybase"]:
-        run(f"docker tag {img_name} {CONTAINER_REGISTRY}/{project}/{img_name}:latest")
-        run(f"docker push {CONTAINER_REGISTRY}/{project}/{img_name}:latest")
+    k8s_target = "kubernetes/"
+    k8s_app_target = "kubernetes/apps"
+    cr = "gcr.io"
 
-    run(f"docker tag flask:{tag} {CONTAINER_REGISTRY}/{project}/flask:{tag}")
-    run(f"docker push {CONTAINER_REGISTRY}/{project}/flask:{tag}")
+    def __init__(self, config, tag, project, models):
+        self.tag = tag
+        self.project = project
+        self.models = models if models and models[0] else None
 
+        # ensure clean path.
+        path = Path(self.k8s_app_target)
+        path.mkdir(exist_ok=True)
+        shutil.rmtree(path / "*", ignore_errors=True)
 
-def build_app_images(obj, project, tag):
-    safeowner = clean(obj["owner"])
-    safetitle = clean(obj["title"])
-    img_name = f"{safeowner}_{safetitle}_tasks"
+        with open(config, "r") as f:
+            self.config = yaml.safe_load(f.read())
 
-    reg_url = "https://github.com"
-    raw_url = "https://raw.githubusercontent.com"
+        with open("flask-deployment.template.yaml", "r") as f:
+            self.flask_template = yaml.safe_load(f.read())
 
-    buildargs = dict(
-        OWNER=obj["owner"],
-        TITLE=obj["title"],
-        BRANCH=obj["branch"],
-        SAFEOWNER=safeowner,
-        SAFETITLE=safetitle,
-        SIM_TIME_LIMIT=obj["sim_time_limit"],
-        REPO_URL=obj["repo_url"],
-        RAW_REPO_URL=obj["repo_url"].replace(reg_url, raw_url),
-        **obj["env"],
-    )
+        with open("app-deployment.template.yaml", "r") as f:
+            self.app_template = yaml.safe_load(f.read())
 
-    buildargs_str = " ".join(
-        [f"--build-arg {arg}={value}" for arg, value in buildargs.items()]
-    )
-    cmd = (
-        f"docker build {buildargs_str} -t {img_name}:{tag} "
-        f"-f dockerfiles/Dockerfile.tasks ./"
-    )
-    run(cmd)
+    def build(self):
+        self.build_base_images()
+        self.write_flask_deployment()
+        self.build_apps()
 
-    tag = obj.get("TAG") or args.tag
-    run(f"docker tag {img_name}:{tag} {CONTAINER_REGISTRY}/{project}/{img_name}:{tag}")
-    run(f"docker push {CONTAINER_REGISTRY}/{project}/{img_name}:{tag}")
+    def apply(self):
+        run("kubectl apply -f {self.k8s_target}")
+        run("kubectl apply -f {self.k8s_app_target}")
 
+    def dry_run(self):
+        self.write_flask_deployment()
+        for app in self.config:
+            for action in ["io", "sim"]:
+                self.write_app_deployment(app, action)
 
-def write_k8s_config(kube_config, obj, project, tag, action):
-    kube_config = copy.deepcopy(kube_config)
-    safeowner = clean(obj["owner"])
-    safetitle = clean(obj["title"])
-    name = f"{safeowner}-{safetitle}-{action}"
+    def build_base_images(self):
+        run("docker build -t distributed:latest -f dockerfiles/Dockerfile ./")
+        run("docker build -t celerybase:latest -f dockerfiles/Dockerfile.celerybase ./")
+        run(f"docker build -t flask:{self.tag} -f dockerfiles/Dockerfile.flask ./")
 
-    if action == "io":
-        resources = {
-            "requests": {"cpu": "700m", "memory": "250Mi"},
-            "limits": {"cpu": "1000m", "memory": "700Mi"},
-        }
-        affinity_size = ["small", "medium"]
-    else:
-        resources = {"requests": {"memory": "1000Mi", "cpu": "1000m"}}
-        resources = dict(resources, **copy.deepcopy(obj["resources"]))
-        affinity_size = obj.get("affinity", {}).get("size", ["small", "medium"])
+        for img_name in ["distributed", "celerybase"]:
+            run(f"docker tag {img_name} {self.cr}/{self.project}/{img_name}:latest")
+            run(f"docker push {self.cr}/{self.project}/{img_name}:latest")
 
-    if not isinstance(affinity_size, list):
-        affinity_size = [affinity_size]
+        run(f"docker tag flask:{self.tag} {self.cr}/{self.project}/flask:{self.tag}")
+        run(f"docker push {self.cr}/{self.project}/flask:{self.tag}")
 
-    kube_config["metadata"]["name"] = name
-    kube_config["spec"]["selector"]["matchLabels"]["app"] = name
-    kube_config["spec"]["template"]["metadata"]["labels"] = name
-    if "affinity" in obj:
-        kube_config["spec"]["template"]["spec"]["affinity"] = {
-            "nodeAffinity": {
-                "requiredDuringSchedulingIgnoredDuringExecution": {
-                    "nodeSelectorTerms": [
-                        {
-                            "matchExpressions": [
-                                # only size for now.
-                                {
-                                    "key": "size",
-                                    "operator": "In",
-                                    "values": affinity_size,
-                                }
-                            ]
-                        }
-                    ]
+    def write_flask_deployment(self):
+        flask_deployment = copy.deepcopy(self.flask_template)
+        flask_deployment["spec"]["template"]["spec"]["containers"][0][
+            "image"
+        ] = f"gcr.io/{self.project}/flask:{self.tag}"
+
+        with open(f"{self.k8s_target}/flask-deployment.yaml", "w") as f:
+            f.write(yaml.dump(flask_deployment))
+
+        return flask_deployment
+
+    def build_apps(self):
+        for app in self.config:
+            try:
+                self.build_app_image(app)
+            except Exception as e:
+                print(
+                    f"There was an error building: "
+                    f"{app['title']}/{app['owner']}:{self.tag}"
+                )
+                print(e)
+                continue
+
+            for action in ["io", "sim"]:
+                self.write_app_deployment(app, action)
+
+    def build_app_image(self, app):
+        safeowner = clean(app["owner"])
+        safetitle = clean(app["title"])
+        img_name = f"{safeowner}_{safetitle}_tasks"
+
+        reg_url = "https://github.com"
+        raw_url = "https://raw.githubusercontent.com"
+
+        buildargs = dict(
+            OWNER=app["owner"],
+            TITLE=app["title"],
+            BRANCH=app["branch"],
+            SAFEOWNER=safeowner,
+            SAFETITLE=safetitle,
+            SIM_TIME_LIMIT=app["sim_time_limit"],
+            REPO_URL=app["repo_url"],
+            RAW_REPO_URL=app["repo_url"].replace(reg_url, raw_url),
+            **app["env"],
+        )
+
+        buildargs_str = " ".join(
+            [f"--build-arg {arg}={value}" for arg, value in buildargs.items()]
+        )
+        cmd = (
+            f"docker build {buildargs_str} -t {img_name}:{self.tag} "
+            f"-f dockerfiles/Dockerfile.tasks ./"
+        )
+        run(cmd)
+
+        run(
+            f"docker tag {img_name}:{self.tag} {self.cr}/{self.project}/{img_name}:{self.tag}"
+        )
+        run(f"docker push {self.cr}/{self.project}/{img_name}:{self.tag}")
+
+    def write_app_deployment(self, app, action):
+        app_deployment = copy.deepcopy(self.app_template)
+        safeowner = clean(app["owner"])
+        safetitle = clean(app["title"])
+        name = f"{safeowner}-{safetitle}-{action}"
+
+        if action == "io":
+            resources = {
+                "requests": {"cpu": "700m", "memory": "250Mi"},
+                "limits": {"cpu": "1000m", "memory": "700Mi"},
+            }
+            affinity_size = ["small", "medium"]
+        else:
+            resources = {"requests": {"memory": "1000Mi", "cpu": "1000m"}}
+            resources = dict(resources, **copy.deepcopy(app["resources"]))
+            affinity_size = app.get("affinity", {}).get("size", ["small", "medium"])
+
+        if not isinstance(affinity_size, list):
+            affinity_size = [affinity_size]
+
+        app_deployment["metadata"]["name"] = name
+        app_deployment["spec"]["selector"]["matchLabels"]["app"] = name
+        app_deployment["spec"]["template"]["metadata"]["labels"] = name
+        if "affinity" in app:
+            app_deployment["spec"]["template"]["spec"]["affinity"] = {
+                "nodeAffinity": {
+                    "requiredDuringSchedulingIgnoredDuringExecution": {
+                        "nodeSelectorTerms": [
+                            {
+                                "matchExpressions": [
+                                    # only size for now.
+                                    {
+                                        "key": "size",
+                                        "operator": "In",
+                                        "values": affinity_size,
+                                    }
+                                ]
+                            }
+                        ]
+                    }
                 }
             }
-        }
 
-    container_config = kube_config["spec"]["template"]["spec"]["containers"][0]
+        container_config = app_deployment["spec"]["template"]["spec"]["containers"][0]
 
-    container_config.update(
-        dict(
-            name=name,
-            image=f"{CONTAINER_REGISTRY}/{project}/{safeowner}_{safetitle}_tasks:{tag}",
-            command=[f"./celery_{action}.sh"],
-            args=[
-                obj["owner"],
-                obj["title"],
-            ],  # TODO: pass safe names to docker file at build and run time
-            resources=resources,
+        container_config.update(
+            dict(
+                name=name,
+                image=f"{self.cr}/{self.project}/{safeowner}_{safetitle}_tasks:{self.tag}",
+                command=[f"./celery_{action}.sh"],
+                args=[
+                    app["owner"],
+                    app["title"],
+                ],  # TODO: pass safe names to docker file at build and run time
+                resources=resources,
+            )
         )
-    )
 
-    container_config["env"].append({"name": "TITLE", "value": obj["title"]})
-    container_config["env"].append({"name": "OWNER", "value": obj["owner"]})
+        container_config["env"].append({"name": "TITLE", "value": app["title"]})
+        container_config["env"].append({"name": "OWNER", "value": app["owner"]})
 
-    # TODO: write secrets to secret config files instead of env.
-    if obj.get("secret"):
-        for var, val in obj.get("secret", {}).items():
-            container_config["env"].append({"name": var.upper(), "value": val})
+        # TODO: write secrets to secret config files instead of env.
+        if app.get("secret"):
+            for var, val in app.get("secret", {}).items():
+                container_config["env"].append({"name": var.upper(), "value": val})
 
-    return kube_config
+        with open(f"{self.k8s_app_target}/{name}-deployment.yaml", "w") as f:
+            f.write(yaml.dump(app_deployment))
 
-
-def read_config_files():
-    with open(args.config, "r") as f:
-        config = yaml.safe_load(f.read())
-
-    with open("flask-deployment.template.yaml", "r") as f:
-        flask_template = yaml.safe_load(f.read())
-
-    with open("app-deployment.template.yaml", "r") as f:
-        kube_template = yaml.safe_load(f.read())
-
-    return config, flask_template, kube_template
-
-
-def write_flask_deployment(flask_template):
-    flask_template["spec"]["template"]["spec"]["containers"][0][
-        "image"
-    ] = f"gcr.io/{args.project}/flask:{args.tag}"
-
-    with open("kubernetes/flask-deployment.yaml", "w") as f:
-        f.write(yaml.dump(flask_template))
+        return app_deployment
 
 
 if __name__ == "__main__":
@@ -167,43 +216,15 @@ if __name__ == "__main__":
     parser.add_argument("--tag", required=False, default=TAG)
     parser.add_argument("--project", required=False, default=PROJECT)
     parser.add_argument("--models", nargs="+", type=str, required=False, default=None)
+    parser.add_argument("--build", action="store_true")
+    parser.add_argument("--dry-run", action="store_true")
     args = parser.parse_args()
 
-    path = Path("kubernetes/apps")
-    path.mkdir(exist_ok=True)
-    shutil.rmtree(path / "*", ignore_errors=True)
+    cluster = ComputeStudioCluster(
+        config=args.config, tag=args.tag, project=args.project, models=args.models
+    )
 
-    config, flask_template, kube_template = read_config_files()
-
-    write_flask_deployment(flask_template)
-
-    build_base_images(args.project, args.tag)
-
-    models = args.models if args.models and args.models[0] else None
-
-    for obj in config:
-        if models and obj["title"] not in models:
-            continue
-        safeowner = clean(obj["owner"])
-        safetitle = clean(obj["title"])
-
-        try:
-            build_app_images(obj, args.project, args.tag)
-        except Exception as e:
-            print(
-                f"There was an error building: {obj['title']}/{obj['owner']}:{args.tag}"
-            )
-            print(e)
-            continue
-
-        for action in ["io", "sim"]:
-            kube_config = write_k8s_config(
-                kube_template, obj, args.project, args.tag, action
-            )
-            name = f"{safeowner}-{safetitle}-{action}"
-            kubeout = (
-                f"kubernetes/test-apps/{safeowner}-{safetitle}-{action}-deployment.yaml"
-            )
-
-            with open(kubeout, "w") as f:
-                f.write(yaml.dump(kube_config))
+    if args.build:
+        cluster.build()
+    elif args.dry_run:
+        cluster.dry_run()
