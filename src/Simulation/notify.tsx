9926dbaccb4b5edf51709a08212bd939df746d0e
hdoupe <henrymdoupe@gmail.com>
2020-01-28 13:53:40 -0500

    Add a for changing the notify on completion field

---
+++ src/Simulation/notify.tsx
@@ -0,0 +1,25 @@
+import * as React from "react";
+
+export const NotifyOnCompletion: React.FC<{ notify: boolean; setNotify: (notify: boolean) => void; }> = ({
+  notify,
+  setNotify
+}) => {
+  console.log(notify);
+  const toggleNotify = () => {
+    setNotify(!notify);
+  };
+  return (
+    // alignment on run model dialog window.
+    <p className="mt-2 mb-1">
+      <input
+        name="notify"
+        className="form-check mr-2"
+        type="checkbox"
+        checked={notify}
+        onChange={toggleNotify}
+        style={{ display: "inline" }}
+      />
+      Notify me on completion
+    </p>
+  );
+};
