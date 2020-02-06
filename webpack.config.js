49ce3efee953ce1fa6564446d330b204947f300b
hdoupe <henrymdoupe@gmail.com>
2020-02-06 15:25:09 -0500

    Add activity app to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,6 @@
 var path = require("path");
 var webpack = require("webpack");
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
-  .BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = [
   {
@@ -38,6 +37,35 @@ module.exports = [
     entry: "./src/Simulation/index.tsx",
     output: {
       filename: "sim.js",
+      path: path.resolve(__dirname, "static/js")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          loader: "ts-loader"
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"]
+        },
+        {
+          enforce: "pre",
+          test: /\.js$/,
+          loader: "source-map-loader"
+        }
+      ]
+    },
+    // plugins: [new BundleAnalyzerPlugin({ analyzerPort: 8002 })],
+    resolve: {
+      extensions: [".ts", ".tsx", ".js"]
+    }
+  },
+  {
+    devtool: "source-map",
+    entry: "./src/Activity/index.tsx",
+    output: {
+      filename: "activity.js",
       path: path.resolve(__dirname, "static/js")
     },
     module: {
