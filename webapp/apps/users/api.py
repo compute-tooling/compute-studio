c2611b46e4303c54faa15b63f044f48460f7249b
hdoupe <henrymdoupe@gmail.com>
2020-02-19 19:04:05 -0500

    Add email to user filter and use smarter sorting order

--- webapp/apps/users/api.py
+++ webapp/apps/users/api.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from django.db.models import Q, BooleanField, Case, When, Value
 from rest_framework.views import APIView
 from rest_framework import serializers, status
 from rest_framework.response import Response
@@ -37,6 +38,23 @@ class UsersAPIView(APIView):
         if not search_term:
             return Response([], status=status.HTTP_200_OK)
 
-        suggested = User.objects.filter(username__icontains=search_term)[:10]
+        suggested = (
+            User.objects.filter(
+                Q(username__icontains=search_term) | Q(email__iexact=search_term)
+            )
+            .annotate(
+                exact_match=Case(
+                    When(username__iexact=search_term, then=Value(True)),
+                    default=Value(False),
+                    output_field=BooleanField(),
+                ),
+                startswith_match=Case(
+                    When(username__istartswith=search_term, then=Value(True)),
+                    default=Value(False),
+                    output_field=BooleanField(),
+                ),
+            )
+            .order_by("-exact_match", "-startswith_match")
+        )[:10]
         results = UserSerializer(suggested, many=True)
         return Response(results.data, status=status.HTTP_200_OK)
