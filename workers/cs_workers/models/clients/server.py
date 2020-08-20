a19ea638bfa330110aeb2d2204a878a9e722803e
hdoupe <henrymdoupe@gmail.com>
2020-08-20 12:07:58 -0400

    Add crud operations for IngressRoute and make deployment name configurable

--- workers/cs_workers/models/clients/server.py
+++ workers/cs_workers/models/clients/server.py
@@ -1,14 +1,14 @@
-import json
 import os
 import redis
 import sys
-import uuid
 import yaml
 
 from kubernetes import client as kclient, config as kconfig
 
+
 from cs_workers.utils import clean, redis_conn_from_env
 from cs_workers.config import ModelConfig
+from cs_workers.ingressroute import IngressRouteApi, ingress_route_template
 
 PORT = 8010
 
@@ -17,6 +17,8 @@ redis_conn = dict(
     password=os.environ.get("REDIS_SCHEDULER_PW"),
     **redis_conn_from_env(),
 )
+
+VIZ_HOST = os.environ.get("VIZ_HOST", "viz.compute.studio")
 
 
 class Server:
@@ -28,6 +30,8 @@ class Server:
         tag,
         model_config,
         callable_name=None,
+        deployment_name="default",
+        namespace="default",
         cr="gcr.io",
         incluster=True,
         rclient=None,
@@ -39,6 +43,8 @@ class Server:
         self.tag = tag
         self.model_config = model_config
         self.callable_name = callable_name
+        self.deployment_name = deployment_name
+        self.namespace = namespace
         self.cr = cr
         self.quiet = quiet
 
@@ -53,9 +59,9 @@ class Server:
             kconfig.load_kube_config()
         self.deployment_api_client = kclient.AppsV1Api()
         self.service_api_client = kclient.CoreV1Api()
-        self.service, self.deployment = self.configure(owner, title, tag, callable_name)
+        self.ir_api_client = IngressRouteApi()
 
-    def env(self, owner, title, config):
+    def env(self, owner=None, title=None, config=None):
         safeowner = clean(owner)
         safetitle = clean(title)
         envs = [
@@ -99,17 +105,18 @@ class Server:
 
         return envs
 
-    def configure(self, owner, title, tag, callable_name):
-        config = self.model_config.projects()[f"{owner}/{title}"]
+    def configure(self):
+        config = self.model_config.projects()[f"{self.owner}/{self.title}"]
+        safeowner = clean(self.owner)
+        safetitle = clean(self.title)
+        app_name = f"{safeowner}-{safetitle}"
+        name = f"{app_name}-{self.deployment_name}"
 
-        safeowner = clean(owner)
-        safetitle = clean(title)
-        name = f"{safeowner}-{safetitle}"
         container = kclient.V1Container(
             name=name,
-            image=f"{self.cr}/{self.project}/{safeowner}_{safetitle}_tasks:{tag}",
-            command=["gunicorn", f"cs_config.functions:{callable_name}"],
-            env=self.env(owner, title, config),
+            image=f"{self.cr}/{self.project}/{safeowner}_{safetitle}_tasks:{self.tag}",
+            command=["gunicorn", f"cs_config.functions:{self.callable_name}"],
+            env=self.env(self.owner, self.title, config),
             resources=kclient.V1ResourceRequirements(**config["resources"]),
             ports=[kclient.V1ContainerPort(container_port=PORT)],
         )
@@ -128,7 +135,7 @@ class Server:
             selector=kclient.V1LabelSelector(match_labels={"app": name}),
             replicas=1,
         )
-        # Instantiate the job object
+        # Instantiate the deployment object
         deployment = kclient.V1Deployment(
             api_version="apps/v1",
             kind="Deployment",
@@ -149,51 +156,124 @@ class Server:
             ),
         )
 
-        # TODO: Update Viz Ingress here.
+        path_prefix = f"/{self.owner}/{self.title}/{self.deployment_name}"
+        routes = [
+            {
+                "kind": "Rule",
+                "match": f"Host(`{VIZ_HOST}`) && PathPrefix(`{path_prefix}`)",
+                "services": [{"name": name, "port": 80}],
+            }
+        ]
+        ingress_route = ingress_route_template(
+            namespace=self.namespace, name=name, routes=routes, tls=True
+        )
 
         if not self.quiet:
             sys.stdout.write(yaml.dump(deployment.to_dict()))
             sys.stdout.write("---\n")
             sys.stdout.write(yaml.dump(service.to_dict()))
 
-        return service, deployment
+        self.service, self.deployment, self.ingress_route = (
+            service,
+            deployment,
+            ingress_route,
+        )
+
+    def deployment_from_cluster(self):
+        try:
+            return self.deployment_api_client.read_namespaced_deployment(
+                self.full_name, self.namespace
+            )
+        except kclient.rest.ApiException as e:
+            if e.reason != "Not Found":
+                raise e
+        return None
+
+    def service_from_cluster(self):
+        try:
+            return self.service_api_client.read_namespaced_service(
+                self.full_name, self.namespace
+            )
+        except kclient.rest.ApiException as e:
+            if e.reason != "Not Found":
+                raise e
+        return None
+
+    def ingressroute_from_cluster(self):
+        try:
+            return self.ir_api_client.get_namespaced_ingressroute(
+                self.full_name, self.namespace
+            )
+        except kclient.rest.ApiException as e:
+            if e.reason != "Not Found":
+                raise e
+        return None
 
     def apply(self):
         try:
-            dep_resp = self.deployment_api_client.create_namespaced_deployment(
-                namespace="default", body=self.deployment
+            deployment_resp = self.deployment_api_client.create_namespaced_deployment(
+                namespace=self.namespace, body=self.deployment
             )
-        except Exception:
-            dep_resp = self.deployment_api_client.patch_namespaced_deployment(
-                self.deployment.metadata.name, namespace="default", body=self.deployment
+        except kclient.rest.ApiException as e:
+            if e.reason != "Not Found":
+                raise e
+            deployment_resp = self.deployment_api_client.patch_namespaced_deployment(
+                self.deployment.metadata.name,
+                namespace=self.namespace,
+                body=self.deployment,
             )
 
-        try:
+        curr_svc = self.service_from_cluster()
+        if not curr_svc:
             service_resp = self.service_api_client.create_namespaced_service(
-                namespace="default", body=self.service
+                namespace=self.namespace, body=self.service
             )
-        except Exception:
-            service_resp = self.service_api_client.patch_namespaced_service(
-                self.service.metadata.name, namespace="default", body=self.service
-            )
+        else:
+            print("Service already exists", curr_svc)
+            service_resp = None
 
-        return dep_resp, service_resp
+        curr_ir = self.ingressroute_from_cluster()
+        if not curr_ir:
+            ingressroute_resp = self.ir_api_client.create_namespaced_ingressroute(
+                namespace=self.namespace, body=self.ingress_route
+            )
+        else:
+            print("IngressRoute already exists:", curr_ir)
+            ingressroute_resp = None
+
+        return deployment_resp, service_resp, ingressroute_resp
 
     def delete(self):
-        return (
+        if self.deployment_from_cluster():
+            print(f"deleting deployment: {self.full_name}")
             self.deployment_api_client.delete_namespaced_deployment(
-                namespace="default", name=self.deployment.metadata.name
-            ),
+                namespace=self.namespace, name=self.deployment.metadata.name
+            )
+        else:
+            print(f"deployment not found: {self.full_name}")
+
+        if self.service_from_cluster():
+            print(f"deleting service: {self.full_name}")
             self.service_api_client.delete_namespaced_service(
-                namespace="default", name=self.service.metadata.name
-            ),
-        )
+                namespace=self.namespace, name=self.service.metadata.name
+            )
+        else:
+            print(f"service not found: {self.full_name}")
+
+        if self.ingressroute_from_cluster():
+            print(f"deleting ingressroute: {self.full_name}")
+            self.ir_api_client.delete_namespaced_ingressroute(
+                name=self.full_name, namespace=self.namespace
+            )
+        else:
+            print(f"ingressroute not found: {self.full_name}")
+        return
 
     @property
     def full_name(self):
         safeowner = clean(self.owner)
         safetitle = clean(self.title)
-        return f"{safeowner}-{safetitle}"
+        return f"{safeowner}-{safetitle}-{self.deployment_name}"
 
 
 if __name__ == "__main__":
@@ -202,15 +282,13 @@ if __name__ == "__main__":
         owner="hdoupe",
         title="ccc-widget",
         tag="fix-iframe-link3",
+        deployment_name="hankdoupe",
         model_config=ModelConfig("cs-workers-dev", "https://dev.compute.studio"),
         callable_name="dash",
         incluster=False,
-        quiet=False,
+        quiet=True,
     )
-    res = server.apply()
-
-    # s.deployment_api_client.create_namespaced_deployment(
-    #     namespace="default", body=s.deployment
-    # )
-
-    # dclient = server.deployment_api_client
+    server
+    server.configure()
+    server.apply()
+    # server.delete()
