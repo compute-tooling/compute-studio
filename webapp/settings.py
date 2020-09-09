7fd0c6af99e40dcd5ce3cc973a188eb2a1d7a93d
Hank Doupe <henrymdoupe@gmail.com>
2020-09-09 13:18:35 -0400

    Decrease default page size to 25 to improve load time on home page

--- webapp/settings.py
+++ webapp/settings.py
@@ -92,7 +92,7 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 50,
+    "PAGE_SIZE": 25,
 }
 
 MIDDLEWARE = [
