03f9a7259de3e057e8af4b307eae58bf3e86daab
hdoupe <henrymdoupe@gmail.com>
2020-09-21 23:01:09 -0400

    checkpoint: Dev site now live with kubernetes - DNS configured with Traefik. - Adds cli args for disk config for the db. - Moves cli from csd to cs

--- deploy/cs_deploy.py
+++ deploy/cs_deploy.py
@@ -1,6 +1,7 @@
 import argparse
 import copy
 import datetime
+import json
 import os
 from pathlib import Path
 import subprocess
@@ -12,7 +13,9 @@ import yaml
 import cs_secrets
 
 
-defaults = dict(TAG=datetime.datetime.now().strftime("%Y-%m-%d"), PROJECT=None)
+defaults = dict(
+    TAG=datetime.datetime.now().strftime("%Y-%m-%d"), PROJECT=None, host=None
+)
 
 secret_vars = [
     "STRIPE_SECRET",
@@ -27,7 +30,12 @@ secret_vars = [
 def load_env():
     config = copy.deepcopy(defaults)
 
-    user_config = {}
+    path = Path("cs-config.yaml")
+    if path.exists():
+        with open(path, "r") as f:
+            user_config = yaml.safe_load(f.read())["webapp"]
+    else:
+        user_config = {}
 
     for var in defaults:
         if os.environ.get(var):
@@ -50,11 +58,12 @@ class Manager:
     cr = "gcr.io"
 
     def __init__(
-        self, tag, use_kind=False, project=None, kubernetes_target="-",
+        self, tag, use_kind=False, project=None, host=None, kubernetes_target="-",
     ):
         self.tag = tag
         self.use_kind = use_kind
         self.project = project
+        self.host = host
         self.kubernetes_target = kubernetes_target
         self.load_templates()
 
@@ -71,6 +80,9 @@ class Manager:
         with open(Path("web-kubernetes") / "web-configmap.yaml") as f:
             self.web_configmap = yaml.safe_load(f.read())
 
+        with open(Path("web-kubernetes") / "web-ingressroute.template.yaml") as f:
+            self.web_ingressroute = yaml.safe_load(f.read())
+
         with open(Path("web-kubernetes") / "db-deployment.yaml") as f:
             self.db_deployment = yaml.safe_load(f.read())
 
@@ -79,7 +91,7 @@ class Manager:
 
     def build(self):
         run(f"docker build -t webbase:latest -f Dockerfile.base ./")
-        run(f"docker build -t web:{self.tag} -f Dockerfile.dev ./")
+        run(f"docker build -t web:{self.tag} -f Dockerfile ./")
 
     def push(self):
         run(f"docker tag web:{self.tag} {self.web_image}")
@@ -95,12 +107,27 @@ class Manager:
     def web_image(self):
         return f"{self.cr}/{self.project}/web:{self.tag}"
 
-    def config(self):
+    def config(self, update_db=False, disk_type="emptydir", disk_args=None):
         self.write_secret()
         self.write_web()
-        self.write_postgres()
+        if update_db:
+            self.write_db(disk_type, disk_args)
 
-    def write_postgres(self):
+    def write_db(self, disk_type="emptyDir", disk_args=None):
+        db_deployment = self.db_deployment
+
+        if isinstance(disk_args, str):
+            disk_args = json.loads(disk_args)
+        elif disk_args is None:
+            disk_args = {}
+        elif isinstance(disk_args, dict):
+            disk_args = disk_args
+        else:
+            raise TypeError("Disk args must be a string, dict, or None.")
+
+        if disk_type is not None:
+            volumes = db_deployment["spec"]["template"]["spec"]["volumes"]
+            volumes[0] = {"name": volumes[0]["name"], disk_type: disk_args}
         self.write_config(self.db_deployment, filename="db-deployment.yaml")
         self.write_config(self.db_service, filename="db-service.yaml")
 
@@ -108,9 +135,15 @@ class Manager:
         web_obj = copy.deepcopy(self.web_deployment_template)
         web_obj["spec"]["template"]["spec"]["containers"][0]["image"] = self.web_image
 
+        if self.host is not None:
+            web_ir = copy.deepcopy(self.web_ingressroute)
+            web_ir["spec"]["routes"][0]["match"] = f"Host(`{self.host}`)"
+
         self.write_config(web_obj, filename="web-deployment.yaml")
         self.write_config(self.web_service, filename="web-service.yaml")
         self.write_config(self.web_configmap, filename="web-configmap.yaml")
+        if self.host is not None:
+            self.write_config(web_ir, filename="web-ingressroute.yaml")
 
     def write_secret(self):
         secret_obj = copy.deepcopy(self.secret_template)
@@ -137,6 +170,7 @@ def manager_from_args(args: argparse.Namespace):
         args.tag,
         getattr(args, "use_kind", False),
         project=args.project,
+        host=args.host,
         kubernetes_target=getattr(args, "out", "-"),
     )
 
@@ -153,18 +187,19 @@ def push(args):
 
 def config(args):
     manager = manager_from_args(args)
-    manager.config()
+    manager.config(
+        update_db=args.update_db, disk_type=args.disk_type, disk_args=args.disk_args
+    )
 
 
 def cli():
-    parser = argparse.ArgumentParser(
-        "CLI for deploying Compute Studio with Kubernetes."
-    )
+    parser = argparse.ArgumentParser("CLI for deploying Compute Studio.")
     user_config = load_env()
     parser.add_argument(
         "--tag", "-t", default=user_config.get("TAG"),
     )
     parser.add_argument("--project", default=user_config.get("PROJECT"))
+    parser.add_argument("--host", default=user_config.get("HOST"))
     subparsers = parser.add_subparsers()
 
     build_parser = subparsers.add_parser("build", description="Create docker images.")
@@ -178,6 +213,15 @@ def cli():
         "config", description="Create kubernetes files."
     )
     config_parser.add_argument("--out", "-o")
+    config_parser.add_argument("--update-db", action="store_true")
+    config_parser.add_argument(
+        "--disk-type",
+        default="emptyDir",
+        help="Disk type as specified here: https://kubernetes.io/docs/concepts/storage/volumes/#types-of-volumes",
+    )
+    config_parser.add_argument(
+        "--disk-args", required=False, help="Arguments for disk type.",
+    )
     config_parser.set_defaults(func=config)
 
     args = parser.parse_args()
